c3c1fb18a823d32bf27cc10335bcd77973108f78
Daniil Kucherenko <daniil.ukraine@gmail.com>
2024-06-02 18:34:04 +0300

    Add button for transaction adding

--- CostTrackerTestTask/ViewController/AddTransactionViewController.swift
+++ CostTrackerTestTask/ViewController/AddTransactionViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class AddTransactionViewController: UIViewController {
     private let transactionAmountField = UITextField()
     private let categoryPicker = UIPickerView()
+    private let addTransaction = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,26 +19,29 @@ class AddTransactionViewController: UIViewController {
     }
     
     private func setupView() {
+        self.view.backgroundColor = .black
+        self.title = "Add transaction"
         setupInputField()
         setupCategoryPicker()
+        setupAddTransactionButton()
     }
     
     private func setupInputField() {
-        transactionAmountField.placeholder = "Enter the transaction amount"
+        transactionAmountField.placeholder = "Enter the transaction amount…"
         transactionAmountField.borderStyle = .roundedRect
         transactionAmountField.layer.cornerRadius = 15
+        
         
         transactionAmountField.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(transactionAmountField)
         
         NSLayoutConstraint.activate([
             transactionAmountField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            transactionAmountField.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 40)
+            transactionAmountField.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 90)
         ])
     }
     
     private func setupCategoryPicker() {
-        categoryPicker.backgroundColor = .systemBackground
         categoryPicker.delegate = self
         categoryPicker.dataSource = self
         categoryPicker.translatesAutoresizingMaskIntoConstraints = false
@@ -45,8 +49,50 @@ class AddTransactionViewController: UIViewController {
         
         NSLayoutConstraint.activate([
             categoryPicker.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            categoryPicker.topAnchor.constraint(equalTo: self.transactionAmountField.topAnchor, constant: 90)
+            categoryPicker.topAnchor.constraint(equalTo: self.transactionAmountField.bottomAnchor),
+            categoryPicker.widthAnchor.constraint(equalToConstant: 200)
         ])
+    }
+    
+    private func setupAddTransactionButton() {
+        addTransaction.setTitle("Add", for: .normal)
+        addTransaction.setTitleColor(.secondaryLabel, for: .normal)
+        addTransaction.layer.borderWidth = 2.0
+        addTransaction.layer.cornerRadius = 10.0
+        addTransaction.layer.borderColor = UIColor.secondarySystemBackground.cgColor
+        addTransaction.backgroundColor = .secondarySystemFill
+        
+        addTransaction.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(addTransaction)
+        
+        addTransaction.addTarget(self, action: #selector(addBtnClicked), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            addTransaction.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            addTransaction.topAnchor.constraint(equalTo: categoryPicker.bottomAnchor, constant: 20),
+            addTransaction.widthAnchor.constraint(equalToConstant: 150)
+        ])
+    }
+    
+    @objc func addBtnClicked() {
+        guard let transactionAmount = Double(transactionAmountField.text ?? ""), transactionAmount > 0 else {
+            presentAnErrorAlert()
+            return
+        }
+        
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    private func presentAnErrorAlert() {
+        let wrongAlertController = UIAlertController(
+            title: "Wrong amount for transaction",
+            message: "Please, enter a valid number (no negative numbers or zero)",
+            preferredStyle: .alert
+        )
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        wrongAlertController.addAction(cancelAction)
+        
+        present(wrongAlertController, animated: true, completion: nil)
     }
 }
 
@@ -65,10 +111,6 @@ extension AddTransactionViewController: UIPickerViewDataSource {
 extension AddTransactionViewController: UIPickerViewDelegate {
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         Category.getCategoryBy(index: row)?.rawValue
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print("Selected item: \(Category.getCategoryBy(index: row)?.rawValue)")
     }
 }
 
