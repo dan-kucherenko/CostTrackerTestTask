8637bd97ffa9e12f1a5a5ffd8912b21e5e3151e1
Daniil Kucherenko <daniil.ukraine@gmail.com>
2024-06-02 22:26:16 +0300

    Implement the stack view

--- CostTrackerTestTask/ViewController/BitcoinViewController.swift
+++ CostTrackerTestTask/ViewController/BitcoinViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 class BitcoinViewController: UIViewController {
     private let btcRateLbl = UILabel()
     private let balanceLbl = UILabel()
+    private let replenishBalanceBtn = UIButton()
+    private lazy var balanceStackView = UIStackView(arrangedSubviews: [balanceLbl, replenishBalanceBtn])
     private let addTransactionBtn = UIButton()
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +22,12 @@ class BitcoinViewController: UIViewController {
     
     // MARK: - Setup view
     private func setupView() {
-        self.setupBtcRateLabel()
-        self.setupBalanceLabel()
-        self.setupTransactionButton()
+        self.view.backgroundColor = .black
+
+        setupBtcRateLabel()
+        setupBalanceLabel()
+        setupTheStackView()
+        setupTransactionButton()
     }
     
     // MARK: - Bitcoin rate label
@@ -31,7 +35,7 @@ class BitcoinViewController: UIViewController {
         Task {
             btcRateLbl.text = "$ \(await getBtcRateData() ?? "0.0")"
         }
-        btcRateLbl.textColor = .cyan
+        btcRateLbl.textColor = .secondaryLabel
         btcRateLbl.font = .systemFont(ofSize: 20)
         
         self.view.addSubview(btcRateLbl)
@@ -43,35 +47,59 @@ class BitcoinViewController: UIViewController {
         ])
     }
     
+    // MARK: - Stack view setup
+    private func setupTheStackView() {
+        balanceStackView.axis = .horizontal
+        balanceStackView.spacing = 8
+        balanceStackView.alignment = .center
+    
+        setupBalanceLabel()
+        setupReplenishButton()
+    
+        balanceStackView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(balanceStackView)
+        
+        NSLayoutConstraint.activate([
+            replenishBalanceBtn.widthAnchor.constraint(equalToConstant: 30),
+            balanceStackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            balanceStackView.topAnchor.constraint(equalTo: btcRateLbl.bottomAnchor, constant: 40)
+        ])
+    }
+    
     // MARK: - Bitcoin Balance label
     private func setupBalanceLabel() {
         balanceLbl.text = "0.0₿"
-        balanceLbl.textColor = .cyan
+        balanceLbl.textColor = .secondaryLabel
         balanceLbl.font = .systemFont(ofSize: 50)
+    }
+    
+    private func setupReplenishButton() {
+        replenishBalanceBtn.setImage(UIImage(systemName: "plus.square"), for: .normal)
+        replenishBalanceBtn.setPreferredSymbolConfiguration(UIImage.SymbolConfiguration(pointSize: 25, weight: .regular), forImageIn: .normal)
+        replenishBalanceBtn.tintColor = .white
         
-        self.view.addSubview(balanceLbl)
-        balanceLbl.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            balanceLbl.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            balanceLbl.topAnchor.constraint(equalTo: btcRateLbl.bottomAnchor, constant: 30),
-        ])
+        replenishBalanceBtn.addTarget(self, action: #selector(replenishBalance), for: .touchUpInside)
     }
     
     // MARK: - Transaction button
     private func setupTransactionButton() {
-        self.addTransactionBtn.setTitle("Add transaction", for: .normal)
-        self.addTransactionBtn.backgroundColor = .blue
-        self.addTransactionBtn.layer.cornerRadius = 10
-        self.addTransactionBtn.addTarget(self, action: #selector(addTransactionClicked), for: .touchUpInside)
-
+        addTransactionBtn.setTitle("Add transaction", for: .normal)
+        addTransactionBtn.titleLabel?.font = .systemFont(ofSize: 20, weight: .semibold)
+        addTransactionBtn.setTitleColor(.secondaryLabel, for: .normal)
+        addTransactionBtn.backgroundColor = .white
+        addTransactionBtn.layer.borderColor = UIColor.secondarySystemBackground.cgColor
+        addTransactionBtn.backgroundColor = .secondarySystemFill
+        addTransactionBtn.layer.cornerRadius = 10
+        
+        addTransactionBtn.addTarget(self, action: #selector(addTransactionClicked), for: .touchUpInside)
+        
         self.view.addSubview(addTransactionBtn)
         addTransactionBtn.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             addTransactionBtn.topAnchor.constraint(equalTo: balanceLbl.bottomAnchor, constant: 20),
             addTransactionBtn.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            addTransactionBtn.widthAnchor.constraint(equalToConstant: 150)
+            addTransactionBtn.widthAnchor.constraint(equalToConstant: 200)
         ])
     }
     
@@ -90,4 +118,29 @@ class BitcoinViewController: UIViewController {
     private func addTransactionClicked() {
         self.navigationController?.pushViewController(AddTransactionViewController(), animated: true)
     }
+    
+    @objc
+    private func replenishBalance() {
+        let replenishBalanceController = UIAlertController(
+            title: "Replenish your balance",
+            message: "Please, enter a valid number (no negative numbers or zero) to add bitcoins for your account",
+            preferredStyle: .alert
+        )
+        replenishBalanceController.addTextField { input in
+            input.placeholder = "Enter amount of bitcoins…"
+            input.keyboardType = .decimalPad
+        }
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        replenishBalanceController.addAction(submitAction)
+        replenishBalanceController.addAction(cancelAction)
+        
+        present(replenishBalanceController, animated: true, completion: nil)
+    }
+}
+
+#Preview {
+    BitcoinViewController()
 }
